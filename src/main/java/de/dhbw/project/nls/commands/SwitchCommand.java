d7ef0f54a7b8c0eeff79bed55a98f3c476fcbdc0
Gerstmeyr Katja <gerstmeyrk.tin19@student.dhbw-heidenheim.de>
2020-09-02 23:21:11 +0200

    feat(switch): expanded switch by on/off

--- src/main/java/de/dhbw/project/nls/commands/SwitchCommand.java
+++ src/main/java/de/dhbw/project/nls/commands/SwitchCommand.java
@@ -10,6 +10,9 @@ public class SwitchCommand extends AutoCommand {
 
     private Game game;
 
+    @PatternName(key = "state")
+    private String state;
+
     @PatternName(key = "item")
     private List<String> item;
 
@@ -20,23 +23,28 @@ public class SwitchCommand extends AutoCommand {
     @Override
     public void execute() {
 
+    	if(state == null || !(state.equals("on") || state.equals("off"))) {
+    		System.out.println("Please tell if you want to switch the item on or off.");
+    		return;
+    	}
+    	
         if (item == null || item.size() == 0) {
             System.out.println("You have to use the whole item name to switch it on or off.");
             return;
         }
 
         String itemName = String.join(" ", item).trim();
-        Item item = game.player.getItem(itemName);
-        if (item == null) {
+        Item switchItem = game.player.getItem(itemName);
+        if (switchItem == null) {
             System.out.println("Item " + itemName + " not found in player inventory.");
-        } else if (item.getItemstate() == ItemState.NOT_USABLE || item.getItemstate() == null) {
+        } else if (switchItem.getItemstate() == ItemState.NOT_USABLE || switchItem.getItemstate() == null) {
             System.out.println("Item " + itemName + " has no state.");
         } else {
-            if (item.getItemstate() == ItemState.ACTIVE) {
-                item.setItemstate(ItemState.INACTIVE);
+            if (state.equals("off")) {
+                switchItem.setItemstate(ItemState.INACTIVE);
                 System.out.println("Item " + itemName + " is now " + ItemState.INACTIVE + ".");
             } else {
-                item.setItemstate(ItemState.ACTIVE);
+                switchItem.setItemstate(ItemState.ACTIVE);
                 System.out.println("Item " + itemName + " is now " + ItemState.ACTIVE + ".");
             }
             game.incTurn();
@@ -45,7 +53,7 @@ public class SwitchCommand extends AutoCommand {
 
     @Override
     public String[] getPattern() {
-        String[] patterns = { "(switch|turn) <item>+", "(switch|turn)" };
+        String[] patterns = { "(switch|turn) <state> <item>+", "(switch|turn) <state>", "(switch|turn)" };
         return patterns;
     }
 }
