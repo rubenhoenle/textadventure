0bc9f0e806884159ed0072e2c768393fb53b8045
Gerstmeyr Katja <gerstmeyrk.tin19@student.dhbw-heidenheim.de>
2020-09-11 09:26:11 +0200

    feat(improve accept): added some error messages

--- src/main/java/de/dhbw/project/nls/commands/AcceptQuestCommand.java
+++ src/main/java/de/dhbw/project/nls/commands/AcceptQuestCommand.java
@@ -24,6 +24,15 @@ public class AcceptQuestCommand extends AutoCommand {
     @Override
     public void execute() {
 
+    	if (quest == null || quest.size() == 0) {
+            System.out.println("You have to tell which quest you want to accept.");
+            return;
+        }
+    	if (questGiver == null || questGiver.size() == 0) {
+            System.out.println("You have to tell from whom you want to accept the quest.");
+            return;
+        }
+
         String characterOrObjectName = String.join(" ", questGiver);
         String questName = String.join(" ", quest);
         Character questCharacter = game.getCharacterFromCurrentRoom(characterOrObjectName);
@@ -31,31 +40,42 @@ public class AcceptQuestCommand extends AutoCommand {
         Quest q = null;
 
         // this block gives the right quest into -> q
-        if (questCharacter != null || questInteractiveObject != null) {
-            if (questCharacter != null && questCharacter instanceof Friend && !questCharacter.isKilled()) {
-                for (int i = 0; i < ((Friend) questCharacter).getQuests().size(); i++) {
-                    if (((Friend) questCharacter).getQuests().get(i).getName().equalsIgnoreCase(questName)) {
-                        q = ((Friend) questCharacter).getQuests().get(i);
-                    }
-                }
-            } else if (questInteractiveObject != null) {
-                q = questInteractiveObject.getQuest();
-            }
+        if (questCharacter != null) {
+        	if(questCharacter instanceof Friend && !questCharacter.isKilled()) {
+	            for (int i = 0; i < ((Friend) questCharacter).getQuests().size(); i++) {
+	                if (((Friend) questCharacter).getQuests().get(i).getName().equalsIgnoreCase(questName)) {
+	                    q = ((Friend) questCharacter).getQuests().get(i);
+	                }
+	            }
+        	} else {
+        		if(questCharacter.isKilled()) {
+        			System.out.println("You cant talk to " + questCharacter.getName() + " because you killed him!!");
+            	} else {
+            		System.out.println("You can accept quests only from friends.");
+            	}
+        		return;
+        	}
+        } else if (questInteractiveObject != null) {
+            q = questInteractiveObject.getQuest();
+        } else {
+        	System.out.println("There is no such character or object!");
+        	return;
+        }
 
-            // checks if the if statement is true and accepts the quest
-            if (q != null && q.isTalkedOnce() && !q.isAccepted() && (questName.equalsIgnoreCase(q.getName()))) {
-                q.setAccepted(true);
-                game.player.getQuestInventory().add(q);
-                System.out.println("Quest \"" + q.getName() + "\" was added to quest inventory");
-                if (questInteractiveObject != null) {
-                    if (null != game.getCurrentRoom().getRoomInteractiveObjectsList()) {
-                        game.getCurrentRoom().getRoomInteractiveObjectsList().remove(questInteractiveObject);
-                    }
+        // checks if the if statement is true and accepts the quest
+        if (q == null) {
+        	System.out.println("There is no such quest!");
+        	return;
+        }
+        	
+        if (q.isTalkedOnce() && !q.isAccepted() && questName.equalsIgnoreCase(q.getName())) {
+            q.setAccepted(true);
+            game.player.getQuestInventory().add(q);
+            System.out.println("Quest \"" + q.getName() + "\" was added to quest inventory");
+            if (questInteractiveObject != null) {
+                if (null != game.getCurrentRoom().getRoomInteractiveObjectsList()) {
+                    game.getCurrentRoom().getRoomInteractiveObjectsList().remove(questInteractiveObject);
                 }
-            } else if (questCharacter != null && questCharacter.isKilled()) {
-                System.out.println("You cant talk to " + questCharacter.getName() + " because you killed him!!");
-            } else {
-                System.out.println("You can not do this!");
             }
         } else {
             System.out.println("You can not do this!");
@@ -64,7 +84,7 @@ public class AcceptQuestCommand extends AutoCommand {
 
     @Override
     public String[] getPattern() {
-        String[] patterns = { "accept <quest>+ from <questGiver>+" };
+        String[] patterns = { "accept <quest>+ from <questGiver>+", "accept <quest>+", "accept" };
         return patterns;
     }
 }
