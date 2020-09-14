ad2d92b07d45058e25d4e023b931651d487052c0
Ruben Hönle <hoenler.tin19@student.dhbw-heidenheim.de>
2020-09-14 07:07:39 +0000

    Merged PR 156: improve command list
    improve command list
    
    Related work items: #2035

--- src/main/java/de/dhbw/project/Constants.java
+++ src/main/java/de/dhbw/project/Constants.java
@@ -14,13 +14,13 @@ public class Constants {
 
     // Possible commands for the game - has to be extended if there are new commands added to the game
     public static final List<String> COMMAND_LIST = new ArrayList<>(Arrays.asList(
-            "move + north / east / west / south / up / down", "look + around / north / east / west / south / up / down",
-            "take + <item name>", "take <item> from <chest>", "put <item> in <chest>", "drop + <item name>",
-            "equip <item name>", "strip off <item name>", "inventory", "equipment", "talk to + <character name>",
-            "attack + <character name>", "accept + <quest> + from + <character name /interactive object name>+",
-            "quest inventory", "switch on/off <item name>", "eat <food name>", "craft <name of crafting object>",
-            "investigate <interactive object name>", "read/open + <readable item>", "translate <stone tablet>",
-            "ruin(s) riddle", "quit or exit", "?", "help or info"));
+            "move north / east / west / south / up / down", "look around / north / east / west / south / up / down",
+            "take <item>", "take <item> from <chest>", "put <item> in <chest>", "drop <item>",
+            "equip <item>", "strip off <item>", "inventory", "equipment", "stats", "talk to <character>",
+            "attack <character>", "accept <quest> from <character / interactive object>",
+            "quest inventory", "switch on/off <item>", "eat <item>", "craft <crafting object>",
+            "investigate <interactive object / chest>", "read / open <item>", "translate <stone tablet>",
+            "ruin(s) riddle", "quit or exit", "?", "map", "help or info"));
 
     // Location of the jsons for a new or a saved game
     public static final String DEFAULT_PATH = "src/main/resources";
